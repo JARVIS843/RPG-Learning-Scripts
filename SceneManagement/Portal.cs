26456e781b685b99f130bd5f4b0c1fd82de37689
Jarvis Yang <46472696+niceTimnice@users.noreply.github.com>
2020-11-04 12:08:16 +0800

    Optimized scene loading system

--- SceneManagement/Portal.cs
+++ SceneManagement/Portal.cs
@@ -12,8 +12,16 @@ namespace RPG.SceneManagement
         private void OnTriggerEnter(Collider other) {
             if(other.tag == "Player")
             {
-                SceneManager.LoadScene(sceneToLoad);
+                StartCoroutine(Transition());
             }
+        }
+
+        private IEnumerator Transition()
+        {
+            DontDestroyOnLoad(gameObject);
+            yield return SceneManager.LoadSceneAsync(sceneToLoad);
+            print("Scene Loaded");
+            Destroy(gameObject);
         }
     }
 }
