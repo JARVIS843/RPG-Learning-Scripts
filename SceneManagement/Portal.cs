31c271c61fc27cce9f47f959ab8f8c58d01ca40e
Jarvis Yang <46472696+niceTimnice@users.noreply.github.com>
2020-11-06 09:38:18 +0800

    Loading Portal Mechanics Done

--- SceneManagement/Portal.cs
+++ SceneManagement/Portal.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -9,6 +10,13 @@ namespace RPG.SceneManagement
     {
 
         [SerializeField] int sceneToLoad = -1;
+        [SerializeField] Transform spawnPoint;
+        [SerializeField] DestinationIdentifier destination;
+
+        enum DestinationIdentifier
+        {
+            A,B,C,D,E
+        }
         private void OnTriggerEnter(Collider other) {
             if(other.tag == "Player")
             {
@@ -18,10 +26,38 @@ namespace RPG.SceneManagement
 
         private IEnumerator Transition()
         {
+            if (sceneToLoad < 0)
+            {
+                Debug.Log("Scene to Load is not set");
+                yield break;
+            }
+
             DontDestroyOnLoad(gameObject);
             yield return SceneManager.LoadSceneAsync(sceneToLoad);
-            print("Scene Loaded");
+            Portal otherPortal = GetOtherPortal();
+            UpdatePlayer(otherPortal);
             Destroy(gameObject);
+        }
+
+        private void UpdatePlayer(Portal otherPortal)
+        {
+            GameObject player = GameObject.FindWithTag("Player");
+            player.transform.position = otherPortal.spawnPoint.transform.position;
+            player.transform.rotation = otherPortal.spawnPoint.transform.rotation;
+        }
+
+        private Portal GetOtherPortal()
+        {
+            foreach(Portal portal in FindObjectsOfType<Portal>())
+            {
+                if (portal == this) continue;
+                if (portal.destination != destination) continue;
+
+
+                return portal;
+            }
+
+            return null;
         }
     }
 }
