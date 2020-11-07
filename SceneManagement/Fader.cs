41b7f3a401121fd811ff7886ad10918ac120be68
Jarvis Yang <46472696+niceTimnice@users.noreply.github.com>
2020-11-07 22:42:23 +0800

    Optimized fading effects between scenes; Fixed minor bugs

--- SceneManagement/Fader.cs
+++ SceneManagement/Fader.cs
@@ -7,21 +7,12 @@ namespace RPG.SceneManagement
         public class Fader : MonoBehaviour
     {
         CanvasGroup canvasGroup;
+        
 
         private void Start() {
             canvasGroup = GetComponent<CanvasGroup>();
-            StartCoroutine(FadeOutIn());
         }
-
-        IEnumerator FadeOutIn()
-        {
-            yield return FadeOut(3f);
-            print("Faded Out");
-            yield return FadeIn(1f);
-            print("Faded In");
-        }
-
-        IEnumerator FadeOut(float time)
+        public IEnumerator FadeOut(float time)
         {
             while(canvasGroup.alpha < 1)
             {
@@ -30,7 +21,7 @@ namespace RPG.SceneManagement
             }
         }
 
-        IEnumerator FadeIn(float time)
+        public IEnumerator FadeIn(float time)
         {
             while(canvasGroup.alpha > 0 )
             {
